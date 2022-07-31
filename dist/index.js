2fb0a2ad82f5511402a3f2adb606a694297375ff
李松明 <2779355293@qq.com>
2022-07-31 14:42:32 +0800

    版本修改

--- dist/index.js
+++ dist/index.js
@@ -55,38 +55,48 @@ __webpack_require__.d(__webpack_exports__, {
 ;// CONCATENATED MODULE: ./src/clipBoard.js
 
 
-const copyEvent = function(ele, content) {
+const copyEvent = function (ele, content) {
   ele.addEventListener('click', () => {
     navigator.clipboard.writeText(content.slice(0, -2)).then(res => {
       navigator.clipboard.readText().then(res => {
-        console.log(res)
+        console.log();
       })
     })
   })
 }
 
-const createClipboard = function({innerData, width = '', height = '', textColor = ''}) {
+const createClipboard = function ({ innerData, width = '', height = '', textColor = '', fontWeight = '700', fontSize = '18' }) {
   let wrap = document.createElement('div');
   let pre = document.createElement('pre');
   let copyIco = document.createElement('span')
 
   pre.innerHTML = innerData;
+  pre.style.cssText = `width: ${width}px;
+                      height: ${height}px;
+                      color: ${textColor};
+                      font-weight: ${fontWeight};
+                      font-size: ${fontSize}px
+                      `;
   wrap.classList.add('wrap');
-  wrap.style.cssText = `width: ${width}px; height: ${height}px; color: ${textColor}; `;
+
   copyIco.innerText = '复制';
   copyIco.classList.add('copy-ico');
-  copyIco.addEventListener('click', function(){
+  copyIco.addEventListener('click', function () {
     this.style.setProperty('background-color', '#aaa')
-    this.style.setProperty('color', 'gray')
+    this.style.setProperty('color', '#eee')
+    this.innerText = '复制成功'
     setTimeout(() => {
-      this.removeEventListener('click')
-    });
+      this.style.setProperty('background-color', '#bbb')
+      this.style.setProperty('color', '#333333')
+      this.innerText = '复制'
+    }, 1000);
   })
-
-  copyEvent(copyIco, wrap.textContent)
 
   wrap.appendChild(pre);
   wrap.appendChild(copyIco);
+
+  copyEvent(copyIco, wrap.textContent)
+
   return wrap;
 }
 
