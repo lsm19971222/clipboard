9217a0a9c61d47695e25b6cda1c6768b5436044a
李松明 <2779355293@qq.com>
2022-07-31 13:14:04 +0800

    功能优化

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ npm install clipboard
 ```
 ##使用
 ```
-const { createClipboard } = require('clipboard')
-import '...'
+const { default: clipboard } = require('clipboard')
+导入样式
+import 'clipboard_zh/dist/styles/index.css'
+```
+
+##参数说明
+```
+接收一个对象
+{
+  innerData,  //想要展示的文本信息
+  width = '', //文本框宽度
+  height = '', //文本框高度
+  textColor = '' //文本字体颜色
+}
 ```
--- src/clipBoard.js
+++ src/clipBoard.js
@@ -1,23 +1,36 @@
 import './index.less'
 
-const createClipboard = function({innerData, width = '', height = '', textColor = ''}) {
-  let wrap = document.createElement('div');
-  let copyIco = document.createElement('span')
-
-  wrap.innerHTML = innerData;
-  wrap.classList.add('wrap');
-  wrap.style.cssText = `width: ${width}px; height: ${height}px; color: ${textColor}; `;
-  copyIco.innerText = '复制';
-  copyIco.classList.add('copy-ico');
-
-  copyIco.addEventListener('click', () => {
-    navigator.clipboard.writeText(wrap.textContent.slice(0, -2)).then(res => {
+const copyEvent = function(ele, content) {
+  ele.addEventListener('click', () => {
+    navigator.clipboard.writeText(content.slice(0, -2)).then(res => {
       navigator.clipboard.readText().then(res => {
         console.log(res)
       })
     })
   })
+}
 
+const createClipboard = function({innerData, width = '', height = '', textColor = ''}) {
+  let wrap = document.createElement('div');
+  let pre = document.createElement('pre');
+  let copyIco = document.createElement('span')
+
+  pre.innerHTML = innerData;
+  wrap.classList.add('wrap');
+  wrap.style.cssText = `width: ${width}px; height: ${height}px; color: ${textColor}; `;
+  copyIco.innerText = '复制';
+  copyIco.classList.add('copy-ico');
+  copyIco.addEventListener('click', function(){
+    this.style.setProperty('background-color', '#aaa')
+    this.style.setProperty('color', 'gray')
+    setTimeout(() => {
+      this.removeEventListener('click')
+    });
+  })
+
+  copyEvent(copyIco, wrap.textContent)
+
+  wrap.appendChild(pre);
   wrap.appendChild(copyIco);
   return wrap;
 }
